37cdb77b94c623725c56382dea38644a29646f0a
andre187erz <apehar21@gmail.com>
2023-09-16 17:37:30 +0200

    Prvi commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    // Show all listings
+    public function index() {
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
+        ]);
+    }
+
+    //Show single listing
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    // Show Create Form
+    public function create() {
+        return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $formFields['user_id'] = auth()->id();
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Oglas uspješno kreiran!');
+    }
+
+        //show edit form
+        public function edit(Listing $listing){
+            return view('listings.edit', ['listing'=>$listing]);
+        }
+
+         //update Listing Data
+    public function update(Request $request, Listing $listing) {
+        //make sure logged in user is owner
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+        
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Oglas uspješno ažuriran!');
+    }
+
+        //delete listing
+        public function destroy(Listing $listing) {
+            if($listing->user_id != auth()->id()) {
+                abort(403, 'Unauthorized Action');
+            }
+            
+            $listing->delete();
+            return redirect('/')->with('message', 'Uspješno obrisano!');
+        }
+        
+        //manage listings
+        public function manage() {
+            return view('listings.manage', ['listings' => auth()->user()->listings()->get()]);
+        }
+
+         
+
+}
---
+++ resources/views/listings/edit.blade.php
@@ -0,0 +1,164 @@
+<x-layout>
+    <x-card class="p-10 rounded max-w-lg mx-auto mt-24"
+                    >
+                        <header class="text-center">
+                            <h2 class="text-2xl font-bold uppercase mb-1">
+                                Uredite!
+                            </h2>
+                            <p class="mb-4">Uredite: {{$listing->title}}</p>
+                        </header>
+    
+                        <form method="POST" action="/listings/{{$listing->id}}" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="mb-6">
+                                <label
+                                    for="company"
+                                    class="inline-block text-lg mb-2"
+                                    >Ime Tvrtke</label
+                                >
+                                <input
+                                    type="text"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="company" value="{{$listing->company}}"
+                                    placeholder="Primjer: Ime Restauranta, i sl."
+                                />
+    
+                                @error('company')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <label for="title" class="inline-block text-lg mb-2"
+                                    >Naziv Posla</label
+                                >
+                                <input
+                                    type="text"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="title" value="{{$listing->title}}"
+                                    placeholder="Primjer: Radnik"
+                                />
+                                @error('title')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <label
+                                    for="location"
+                                    class="inline-block text-lg mb-2"
+                                    >Lokacija Posla</label
+                                >
+                                <input
+                                    type="text"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="location" value="{{$listing->location}}"
+                                    placeholder="Primjer: Mostar BA, etc"
+                                />
+                                @error('location')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <label for="email" class="inline-block text-lg mb-2"
+                                    >Kontakt Email</label
+                                >
+                                <input
+                                    type="text"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="email" value="{{$listing->email}}"
+                                    placeholder="Primjer: imeprezime@gmail.com"
+                                />
+                                @error('email')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <label
+                                    for="website"
+                                    class="inline-block text-lg mb-2"
+                                >
+                                    URL Web-stranice ili Aplikacije
+                                </label>
+                                <input
+                                    type="text"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="website" value="{{$listing->website}}"
+                                    placeholder="Primjer: https://website.com"
+                                />
+                                @error('website')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <label for="tags" class="inline-block text-lg mb-2">
+                                    Oznake (Razdijeljene Zarezom)
+                                </label>
+                                <input
+                                    type="text"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="tags" value="{{$listing->tags}}"
+                                    placeholder="Primjer: Restaurant, Food, Delivery, etc"
+                                />
+                                @error('tags')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <label for="logo" class="inline-block text-lg mb-2">
+                                    Logo Tvrtke
+                                </label>
+                                <input
+                                    type="file"
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="logo"
+                                />
+
+                                <img
+                            class="w-48 mr-6 mb-6"
+                            src="{{$listing->logo ? asset('storage/' . $listing->logo
+                            ) : asset('/images/no-image.png')}}"
+                            alt=""
+                        />
+
+                                @error('logo')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+                            
+                            <div class="mb-6">
+                                <label
+                                    for="description"
+                                    class="inline-block text-lg mb-2"
+                                >
+                                    Opis Posla
+                                </label>
+                                <textarea
+                                    class="border border-gray-200 rounded p-2 w-full"
+                                    name="description"
+                                    rows="10"
+                                    placeholder="Primjer:Zahtjevi, plaća, etc">
+                                    {{$listing->description}}
+                            </textarea>
+                                @error('description')
+                                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                                @enderror
+                            </div>
+    
+                            <div class="mb-6">
+                                <button
+                                    class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
+                                >
+                                    Ažuriraj
+                                </button>
+    
+                                <a href="/" class="text-black ml-4"> Natrag </a>
+                            </div>
+                        </form>
+    </x-card>
+    </x-layout>
